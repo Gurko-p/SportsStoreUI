059b349a7c3992645eefb3c9512e8eb7f64f4945
Pavel Gurko <gurko-p@mail.ru>
2024-09-22 23:33:49 +0300

    fixed html layout in header and CartIcon component

---
+++ front/src/components/cart/CartIcon.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import { itemCountInCart } from "../../features/auth/authSlice";
+import { FaShoppingCart } from "react-icons/fa";
+import { useNavigate, Outlet } from "react-router-dom";
+
+export default function CartIcon() {
+  const { length } = useSelector(itemCountInCart);
+  const navigate = useNavigate();
+  return (
+    <div
+      style={{
+        position: "relative",
+        display: "inline-block",
+        cursor: "pointer",
+      }}
+      onClick={() => navigate("/cart", { replace: true })}
+    >
+      <FaShoppingCart size={30} />
+      {length > 0 && (
+        <span
+          style={{
+            position: "absolute",
+            top: "-5px",
+            right: "-10px",
+            background: "red",
+            color: "white",
+            borderRadius: "50%",
+            padding: "5px 10px",
+            fontSize: "12px",
+          }}
+        >
+          {length}
+        </span>
+      )}
+    </div>
+  );
+}
--- front/src/components/layout/MainLayout.js
+++ front/src/components/layout/MainLayout.js
@@ -1,33 +1,62 @@
-import React from 'react';
-import { AppBar, Toolbar, Typography, Button } from '@mui/material';
-import { useDispatch } from "react-redux"
-import { removeLoggedIn, itemCountInCart } from "../../features/auth/authSlice"
-import { useNavigate, Outlet } from "react-router-dom"
-import Cart from '../cart/Cart'
+import React from "react";
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Button,
+  Box,
+  Divider,
+} from "@mui/material";
+import { useDispatch } from "react-redux";
+import { removeLoggedIn } from "../../features/auth/authSlice";
+import { useNavigate, Outlet } from "react-router-dom";
+import CartIcon from "../cart/CartIcon";
 
 export default function MainLayout() {
-    const navigate = useNavigate();
-    const dispatch = useDispatch()
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
 
-    
-    const logout = () => {
-        dispatch(removeLoggedIn())
-        navigate("/login", { replace: true })
-    }
+  const logout = () => {
+    dispatch(removeLoggedIn());
+    navigate("/login", { replace: true });
+  };
 
-    return (
-        <div>
-            <AppBar position="static">
-                <Toolbar>
-                    <Typography variant="h6">SportsStore</Typography>
-                    <Button color="white" onClick={() => navigate('/about')}>About</Button>
-                    <Button color="white" onClick={() => navigate('/')}>Products</Button>
-                    <Button color="white" onClick={() => navigate('/register')}>Register</Button>
-                    <Button color="inherit" onClick={logout}>Logout</Button>
-                    <Cart></Cart>
-                </Toolbar>
-            </AppBar>
-            <Outlet/>
-        </div>
-    );
-};
+  return (
+    <div>
+      <AppBar position="static">
+        <Toolbar sx={{ display: "flex", justifyContent: "space-between" }}>
+          <div
+            style={{
+              minWidth: "50vh",
+              display: "flex"
+            }}
+          >
+            <div style={{
+              marginRight: '30px'
+            }}>
+              <Typography variant="h6">SportsStore</Typography>
+            </div>
+            <div>
+                <Button color="white" onClick={() => navigate("/")}>
+                  Home
+                </Button>
+                <Button color="white" onClick={() => navigate("/about")}>
+                  About
+                </Button>
+                <Button color="white" onClick={() => navigate("/register")}>
+                  Register
+                </Button>
+              </div>
+          </div>
+          <div style={{ minWidth: "150px", display: "flex" }}>
+            <Button color="white" sx={{ marginRight: "50px" }} onClick={logout}>
+              Logout
+            </Button>
+            <CartIcon />
+          </div>
+        </Toolbar>
+      </AppBar>
+      <Outlet />
+    </div>
+  );
+}
