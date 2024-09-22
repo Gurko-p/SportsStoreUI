3232031617cae1a23c87ac2c4720814a37be656d
Pavel Gurko <gurko-p@mail.ru>
2024-09-22 22:39:51 +0300

    added cart icon in Cart component

--- front/src/components/cart/Cart.js
+++ front/src/components/cart/Cart.js
@@ -1,13 +1,33 @@
 import React from "react";
 import { useSelector } from "react-redux"
 import { itemCountInCart } from "../../features/auth/authSlice"
+import { FaShoppingCart } from 'react-icons/fa';
 
 export default function Cart() {
     const { length } = useSelector(itemCountInCart);
 
     return (
-        <div>
-            <button>{length}</button>
+        <div style={{ position: 'relative', display: 'inline-block', cursor: 'pointer' }}>
+            <FaShoppingCart size={30} />
+            {length > 0 && (
+                <span style={{
+                    position: 'absolute',
+                    top: '-5px',
+                    right: '-10px',
+                    background: 'red',
+                    color: 'white',
+                    borderRadius: '50%',
+                    padding: '5px 10px',
+                    fontSize: '12px'
+                }}>
+                    {length}
+                </span>
+            )}
         </div>
+
+
+        // <div>
+        //     <button>{length}</button>
+        // </div>
     )
 }
